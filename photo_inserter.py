a7e8dbf744456b7316e7863dccc24f14438ac960
Ansh Jhaveri <anshj2003@gmail.com>
2024-07-18 00:17:38 -0400

    lat long update

--- photo_inserter.py
+++ photo_inserter.py
@@ -1,17 +1,30 @@
 import pandas as pd
 import psycopg2
+import ast
 
 # Database connection details
 db_config = {
-    'dbname': 'intoit-prod',
+    'dbname': 'nightlife_st',
     'user': 'postgres',
     'password': 'Anshtheboss1',
-    'host': 'intoit-prod.cx2s40qaqixr.us-east-1.rds.amazonaws.com',
+    'host': '3.229.18.161',
 }
 
 # Read the CSV file
-csv_file_path = '/Users/anshjhaveri/downloads/places_with_images.csv'
+csv_file_path = '/Users/anshjhaveri/downloads/updated_csv_file_geoparse.csv'
 data = pd.read_csv(csv_file_path)
+
+# Define a function to extract latitude and longitude from the geometry column
+def extract_lat_lng(geometry):
+    try:
+        geom_dict = ast.literal_eval(geometry)
+        location = geom_dict.get('location', {})
+        return location.get('lat', None), location.get('lng', None)
+    except (ValueError, SyntaxError):
+        return None, None
+
+# Apply the function to the geometry column and update latitude and longitude columns
+data['latitude'], data['longitude'] = zip(*data['geometry'].apply(extract_lat_lng))
 
 # Establish a database connection
 conn = psycopg2.connect(**db_config)
@@ -20,15 +33,16 @@ cursor = conn.cursor()
 # Update the database records
 for index, row in data.iterrows():
     name = row['name']
-    photo = row['photo']
+    latitude = row['latitude']
+    longitude = row['longitude']
     # Construct the SQL query
     query = """
     UPDATE bars
-    SET photo = %s
+    SET latitude = %s, longitude = %s
     WHERE name = %s;
     """
-    cursor.execute(query, (photo, name))
-    print(f"Updated {name} with photo {photo}")
+    cursor.execute(query, (latitude, longitude, name))
+    print(f"Updated {name} with latitude {latitude} and longitude {longitude}")
 
 # Commit the changes and close the connection
 conn.commit()
